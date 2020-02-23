f6787842345d36d1909803a96d0862dc073d238e
Nonononoki <nonononoki@gmail.com>
2020-02-23 17:10:06 +0100

    Alert on failed login

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -4,9 +4,9 @@ $(function() {
 	//TODO
 	if(url.includes("?confirm-registration")) {
 		alert("A confirmation email has been sent to your email address.")
-	} else if("?registration-confirm-success") {
+	} else if(url.includes("?registration-confirm-success")) {
 		alert("Confirmation succeeded, you can now login and meet new people in your area!")
-	} else if("?registration-confirm-failed") {
+	} else if(url.includes("?registration-confirm-failed")) {
 		alert("Confirmation failed, please try again!")
 	}
 });
---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,9 @@
+$(function() {
+	let url = window.location.href;
+	
+	//TODO
+	if(url.includes("?error")) {
+		alert("Login failed, please check your credentials, complete the captcha and try again. \n" +
+				"A new confirmation email has been sent to you if your account hasn't been verified yet.")
+	} 
+});
