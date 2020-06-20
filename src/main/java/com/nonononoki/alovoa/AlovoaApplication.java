7b88dc9ce47e1c94947ae667d9c3ba9c56f964b3
Nonononoki <nonononoki@gmail.com>
2020-06-20 19:46:53 +0200

    Initial bucket4j implementation

--- src/main/java/com/nonononoki/alovoa/AlovoaApplication.java
+++ src/main/java/com/nonononoki/alovoa/AlovoaApplication.java
@@ -6,11 +6,14 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
 @EnableJpaRepositories("com.nonononoki.alovoa.repo")
 @EntityScan("com.nonononoki.alovoa.entity")
+@EnableCaching
 public class AlovoaApplication {
 
 	public static void main(String[] args) {
