e5e324d42b3f8aedd447a608c22f923574f25036
Nonononoki <nonononoki@gmail.com>
2021-03-28 14:11:14 +0200

    Redirect homepage to profile if authenticated

--- src/main/java/com/nonononoki/alovoa/html/IndexResource.java
+++ src/main/java/com/nonononoki/alovoa/html/IndexResource.java
@@ -1,14 +1,29 @@
 package com.nonononoki.alovoa.html;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.nonononoki.alovoa.entity.User;
+import com.nonononoki.alovoa.service.AuthService;
+
 @Controller
 public class IndexResource {
-	
+
+	@Autowired
+	private AuthService authService;
+
+	@Autowired
+	private ProfileResource profileResource;
+
 	@GetMapping("/")
-	public ModelAndView register() {
+	public ModelAndView index() throws Exception {
+
+		User user = authService.getCurrentUser();
+		if (user != null) {
+			return profileResource.profile();
+		}
 
 		ModelAndView mav = new ModelAndView("index");
 		return mav;
