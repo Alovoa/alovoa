b0d9fcb849a94e8bbd490374f5ea07b2616a6de6
Nho Quy Dinh <nonononoki@gmail.com>
2021-04-01 20:33:37 +0200

    Changed theme color to a colorful pink, may revert later, idk

--- src/main/java/com/nonononoki/alovoa/service/CaptchaService.java
+++ src/main/java/com/nonononoki/alovoa/service/CaptchaService.java
@@ -32,7 +32,8 @@ public class CaptchaService {
 	private final int HEIGHT = 70;
 	
 	private final  Color BG_COLOR = new Color(0, 0, 0, 0);
-	private final  Color FG_COLOR = new Color(41, 182, 246);
+//	private final  Color FG_COLOR = new Color(41, 182, 246);
+	private final  Color FG_COLOR = new Color(236, 65, 122);
 
 	public Captcha generate() throws Exception {
 		OxCaptcha ox = generateCaptchaImage(captchaLength);
