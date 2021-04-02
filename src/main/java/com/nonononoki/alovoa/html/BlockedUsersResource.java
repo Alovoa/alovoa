c82ddf0d081634b3ba273ccdf100e423e64a3f35
Nho Quy Dinh <nonononoki@gmail.com>
2021-04-02 13:19:34 +0200

    Show blocked users in profile

---
+++ src/main/java/com/nonononoki/alovoa/html/BlockedUsersResource.java
@@ -0,0 +1,41 @@
+package com.nonononoki.alovoa.html;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.nonononoki.alovoa.component.TextEncryptorConverter;
+import com.nonononoki.alovoa.entity.User;
+import com.nonononoki.alovoa.entity.user.UserBlock;
+import com.nonononoki.alovoa.model.UserDto;
+import com.nonononoki.alovoa.service.AuthService;
+
+@Controller
+public class BlockedUsersResource {
+	
+	@Autowired
+	private AuthService authService;
+	
+	@Autowired
+	private TextEncryptorConverter textEncryptor;
+	
+	@GetMapping("/blocked-users")
+	public ModelAndView profileView() throws NumberFormatException, Exception {
+		
+		User user = authService.getCurrentUser();
+		ModelAndView mav = new ModelAndView("blocked-users");
+		List<UserBlock> userBlocks = user.getBlockedUsers();
+		List<User> blockedUsers = userBlocks.stream().map(b -> b.getUserTo()).collect(Collectors.toList());
+		List<UserDto> users = new ArrayList<>();
+		for(User u : blockedUsers) {
+			users.add(UserDto.userToUserDto(u, user, textEncryptor, UserDto.PROFILE_PICTURE_ONLY));
+		}
+		mav.addObject("users", users);
+		return mav;	
+	}
+}
