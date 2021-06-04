35a4fc3714e7222b4211e99a6233be985845aeb9
Nho Quy Dinh <nonononoki@gmail.com>
2021-06-04 21:44:59 +0200

    Update FAQ to include steps for data deletion

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export JASYPT_ENCRYPTOR_PASSWORD=secret
 cd ..
-java -jar -Dspring.profiles.active=test target/alovoa-1.0.0.jar > /dev/null
-sleep 20
+java -jar -Dspring.profiles.active=prod target/alovoa-1.0.0.jar > /dev/null
+sleep 10
 unset JASYPT_ENCRYPTOR_PASSWORD
