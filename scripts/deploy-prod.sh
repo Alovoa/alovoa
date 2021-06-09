f89b83e45ebf119f787ed978dbcc80ef8704356c
Nho Quy Dinh <nonononoki@gmail.com>
2021-06-09 22:42:26 +0200

    Better deploy script

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-export JASYPT_ENCRYPTOR_PASSWORD=secret
-cd ..
-java -jar -Dspring.profiles.active=prod target/alovoa-1.0.0.jar > /dev/null
-sleep 10
+read -sp 'Password: ' pw
+cd ../target
+fuser -k 8843/tcp
+export JASYPT_ENCRYPTOR_PASSWORD=$pw
+nohup java -jar -Dspring.profiles.active=prod alovoa-1.0.0.jar &
+sleep 5
 unset JASYPT_ENCRYPTOR_PASSWORD
