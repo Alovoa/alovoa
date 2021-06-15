513011d690d9dfc811f86886a3eb2d12a6610b8a
Nho Quy Dinh <nonononoki@gmail.com>
2021-06-15 19:48:33 +0200

    Show message-warning inside an alert. Several UI fixes.

--- src/main/resources/static/js/alovoa.js
+++ src/main/resources/static/js/alovoa.js
@@ -1,7 +1,5 @@
-window.addEventListener('load', () => { 
-  document.querySelector(':root').style
+document.querySelector(':root').style
     .setProperty('--vh', window.innerHeight/100 + 'px');
-})
 
 window.addEventListener('resize', () => { 
   document.querySelector(':root').style
