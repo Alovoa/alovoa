9cad7ebcf02de5b7a4136ca225ee3acfd0f5a9a4
Nho Quy Dinh <nonononoki@gmail.com>
2021-07-13 21:03:24 +0200

    Small Service Worker Fix

--- src/main/resources/static/sw.js
+++ src/main/resources/static/sw.js
@@ -30,7 +30,7 @@ const staticAssets = [
 	'/js/lib/jquery.min.js',
 	'/js/lib/Mp3LameEncoder.min.js',
 	'/js/lib/notification.js',
-	'/js/lib/swiper.all.js',
+	'/js/lib/swiper.min.js',
 	'/js/lib/leaflet.min.js',
 
 	'/js/tools/check-password.js',
@@ -98,11 +98,11 @@ const staticAssets = [
 ];
 
 if ('serviceWorker' in navigator) {
-	// console.log('Loading Service Worker...')
+	console.log('Loading Service Worker...')
 	window.addEventListener('load', function() {
 		navigator.serviceWorker.register('/sw.js').then(function(registration) {
 			// Registration was successful
-			// console.log('ServiceWorker registration successful with scope: ', registration.scope);
+			console.log('ServiceWorker registration successful with scope: ', registration.scope);
 		}, function(err) {
 			// registration failed :(
 			console.log('ServiceWorker registration failed: ', err);
