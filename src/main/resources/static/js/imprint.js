b9d992ba34e87cd3d1f6371f67bbdb60d210ae6a
Nho Quy Dinh <nonononoki@gmail.com>
2022-03-13 20:02:06 +0100

    Fix imprint

--- src/main/resources/static/js/imprint.js
+++ src/main/resources/static/js/imprint.js
@@ -1,33 +1,33 @@
 $(function() {
 	$("#imprint-form").submit(
-			function(e) {
-				e.preventDefault();
+		function(e) {
+			e.preventDefault();
 
-				var actionUrl = e.currentTarget.action;
-				let formdata = $("#imprint-form").serializeArray().reduce(
-						function(a, x) {
-							a[x.name] = x.value;
-							return a;
-						}, {});
+			var actionUrl = e.currentTarget.action;
+			let formdata = $("#imprint-form").serializeArray().reduce(
+				function(a, x) {
+					a[x.name] = x.value;
+					return a;
+				}, {});
 
-				$.ajax({
-					url : actionUrl,
-					headers : {
-						"X-CSRF-TOKEN" : $("input[name='_csrf']").val()
-					},
-					type : 'POST',
-					data : JSON.stringify(formdata),
-					contentType : "application/json",
-					success : function(e) {
-						alert(getText("success.generic"));
-						location.reload();
-					},
-					error : function(e) {
-						refreshCaptcha();
-						console.log(e);
-						alert(e.responseText);
-					}
-				});
-
+			$.ajax({
+				url: actionUrl,
+				headers: {
+					"X-CSRF-TOKEN": $("input[name='_csrf']").val()
+				},
+				type: 'POST',
+				data: JSON.stringify(formdata),
+				contentType: "application/json",
+				success: function(e) {
+					document.getElementById("imprint-form").reset();
+					alert(getText("success.generic"));
+				},
+				error: function(e) {
+					refreshCaptcha();
+					console.log(e);
+					alert(e.responseText);
+				}
 			});
+
+		});
 });
