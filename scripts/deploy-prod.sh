77bb604598bc3b183fdc1aac2228a70f882b574c
Nho Quy Dinh <nonononoki@gmail.com>
2022-04-06 21:46:19 +0200

    Add UTF-8 parameter to prod script

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -6,6 +6,6 @@ cd target
 read -sp 'Password: ' pw
 fuser -k 8843/tcp
 export JASYPT_ENCRYPTOR_PASSWORD=$pw
-nohup java -XX:+HeapDumpOnOutOfMemoryError -Xmx1024m -jar -Dspring.profiles.active=prod alovoa-1.0.0.jar &
+nohup java -XX:+HeapDumpOnOutOfMemoryError -Xmx1024m -jar -Dfile.encoding=UTF-8 -Dspring.profiles.active=prod alovoa-1.0.0.jar &
 sleep 5
 unset JASYPT_ENCRYPTOR_PASSWORD
