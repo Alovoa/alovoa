d541c4b0ff4c96fc1c68db98b29c0f8548e74219
Nho Quy Dinh <nonononoki@gmail.com>
2022-04-25 22:02:53 +0200

    Improve donate-list layout

--- src/main/resources/static/js/donate-list.js
+++ src/main/resources/static/js/donate-list.js
@@ -4,3 +4,14 @@ $(function() {
 		alert(getText("donate-list.store.warning"));
 	}
 });
+
+function kofiInfo() {
+	let text = document.getElementById("donate-notice-1").innerHTML;
+	let text2 = document.getElementById("donate-notice-2").innerHTML;
+	alert(text);
+	alert(text2);
+}
+
+function bmacInfo() {
+	kofiInfo();
+}
