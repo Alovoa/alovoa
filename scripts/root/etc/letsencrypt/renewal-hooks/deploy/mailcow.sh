5e95615818c0f17d3d7356270ef20da116d4d8f3
Nho Quy Dinh <nonononoki@gmail.com>
2022-06-14 10:39:17 +0200

    Update mailcow cert renewal script

--- scripts/root/etc/letsencrypt/renewal-hooks/deploy/mailcow.sh
+++ scripts/root/etc/letsencrypt/renewal-hooks/deploy/mailcow.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cp /etc/letsencrypt/live/alovoa.com/fullchain.pem /opt/mailcow-dockerized/data/assets/ssl/cert.pem
 cp /etc/letsencrypt/live/alovoa.com/privkey.pem /opt/mailcow-dockerized/data/assets/ssl/key.pem
+cp /etc/letsencrypt/live/alovoa.com/fullchain.pem /opt/mailcow-dockerized/data/assets/ssl/mail.alovoa.com/cert.pem
+cp /etc/letsencrypt/live/alovoa.com/privkey.pem /opt/mailcow-dockerized/data/assets/ssl/mail.alovoa.com/key.pem
 postfix_c=$(docker ps -qaf name=postfix-mailcow)
 dovecot_c=$(docker ps -qaf name=dovecot-mailcow)
 nginx_c=$(docker ps -qaf name=nginx-mailcow)
