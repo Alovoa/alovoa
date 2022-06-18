ee653d274e68b10211c67be786ce12eef6df3e85
Nho Quy Dinh <nonononoki@gmail.com>
2022-06-18 22:02:45 +0200

    New loader

--- src/main/resources/static/js/tools/loader.js
+++ src/main/resources/static/js/tools/loader.js
@@ -1,23 +1,28 @@
 const MIN_LOADER_TIME = 400;
 var loaderTime;
+var loaderNum = 0
 
 function showLoader() {
+	loaderNum = loaderNum + 1
 	loaderTime = new Date().getTime();
 	$(".loader-parent").css("display", "flex");
 }
 
 function hideLoader() {
-	if(!loaderTime) {
-		$(".loader-parent").css("display", "none");
-	} else {
-		let diffTime = new Date().getTime() - loaderTime;
-		console.log(diffTime);
-		if(diffTime >= MIN_LOADER_TIME) {
+	loaderNum = loaderNum - 1
+	if (loaderNum <= 0) {
+		if(!loaderTime) {
 			$(".loader-parent").css("display", "none");
 		} else {
-			setTimeout(function() {
-			  $(".loader-parent").css("display", "none");
-			}, MIN_LOADER_TIME - diffTime);
+			let diffTime = new Date().getTime() - loaderTime;
+			console.log(diffTime);
+			if(diffTime >= MIN_LOADER_TIME) {
+				$(".loader-parent").css("display", "none");
+			} else {
+				setTimeout(function() {
+				  $(".loader-parent").css("display", "none");
+				}, MIN_LOADER_TIME - diffTime);
+			}
 		}
 	}
 }
