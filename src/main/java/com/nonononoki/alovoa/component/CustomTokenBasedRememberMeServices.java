dab12a05cb306f3ac5528a6bbb97fb9c43aca1b0
eddyhanderson <edikativa@gmail.com>
2023-04-05 20:14:57 +0100

    Upgrade Spring Boot version from 2.7.5 to 3.0.0
    The main changes was:
    1 - Spring Security
      1.1 - Change deprecated method authorizeRequest to authorizeHttpRequests
      1.2 - Change method rename from antMatchers to requestMatchers
    
    2 - Spring Data
      2.1 - In Hibernate 6 which is used by Spring 3, Ids are not generated (when using SEQUENCE strategy) using a unique sequence for all entities (called hibernate_sequence), instead it uses sequences for each specific entity. Because of that, it was creating duplicated Ids. To solve that, we use property **spring.jpa.properties.hibernate.id.db_structure_naming_strategy=single** to force hibernate using hibernate_sequence.
    
    3 - Java version
      3.1 - Spring boot 3 requires Java 17+, so we need to change all javax references to jakarta
    
    4 - Docker images
      4.1 - We need to change Spring container base image, to use Java 17+
      4.2 - I think that it will be necessary to upgrade the JRE of the application server

--- src/main/java/com/nonononoki/alovoa/component/CustomTokenBasedRememberMeServices.java
+++ src/main/java/com/nonononoki/alovoa/component/CustomTokenBasedRememberMeServices.java
@@ -2,9 +2,9 @@ package com.nonononoki.alovoa.component;
 
 import java.util.Date;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
--- src/main/java/com/nonononoki/alovoa/component/SuccessHandler.java
+++ src/main/java/com/nonononoki/alovoa/component/SuccessHandler.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
