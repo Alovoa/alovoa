ae7ee24b0157722a5034f7f4d5fb6bf22b8cb87e
Edianderson Kativa <60460379+Eddyhanderson@users.noreply.github.com>
2023-04-10 12:25:40 +0100

    fix: Change java version in build job (#273)
    * Change java version in build job
    
    * fix: Changing scripts app artifact version
    
    * fix: using sequence table per entity in alovoa_test db
    
    There are some tests that are expecting statics ids for entities, like "UserIntetion" entity. And when it's created with a different id, it throws a Exception and fails the test. So, by using 'standard strategy', it'll create for every entity a sequence table, matching the ids that the test is expecting.
    
    *Note:* this was caused due to upgrade of hibernate, the changes in generations of ids.

--- scripts/staging-deploy-prod.sh
+++ scripts/staging-deploy-prod.sh
@@ -6,6 +6,6 @@ cd target
 read -sp 'Password: ' pw
 fuser -k 8844/tcp
 export JASYPT_ENCRYPTOR_PASSWORD=$pw
-nohup java -XX:+HeapDumpOnOutOfMemoryError -Xmx128m -jar -Dfile.encoding=UTF-8 -Dspring.profiles.active=prod alovoa-1.0.0.jar &
+nohup java -XX:+HeapDumpOnOutOfMemoryError -Xmx128m -jar -Dfile.encoding=UTF-8 -Dspring.profiles.active=prod alovoa-1.1.0.jar &
 sleep 5
 unset JASYPT_ENCRYPTOR_PASSWORD
