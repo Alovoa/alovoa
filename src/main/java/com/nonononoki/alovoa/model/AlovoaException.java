c04949fab082d6174d6d179f0f224e5b8ecad1b8
Christian Felsing <christian@felsing.net>
2023-08-23 09:33:00 +0200

    Fixed translation typo

--- src/main/java/com/nonononoki/alovoa/model/AlovoaException.java
+++ src/main/java/com/nonononoki/alovoa/model/AlovoaException.java
@@ -1,11 +1,19 @@
 package com.nonononoki.alovoa.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serial;
+
 public class AlovoaException extends Exception {
+	private static final Logger logger = LoggerFactory.getLogger(AlovoaException.class);
+	@Serial
 	private static final long serialVersionUID = 3421165817331537192L;
 	
 	public static final String MAX_MEDIA_SIZE_EXCEEDED = "max_media_size_exceeded";
 	
 	public AlovoaException(String errorMessage) {
-        super(errorMessage);
+		super(errorMessage);
+		logger.error(String.format("AlovoaException: %s: %s", this.getClass().getName(), errorMessage));
     }
 }
