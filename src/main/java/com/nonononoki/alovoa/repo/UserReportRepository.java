560542217b258f36acda2f92dde6d52eb2fe2cfa
Nho Quy Dinh <nonononoki@gmail.com>
2024-01-18 18:26:02 +0100

    Remove invalid reports

--- src/main/java/com/nonononoki/alovoa/repo/UserReportRepository.java
+++ src/main/java/com/nonononoki/alovoa/repo/UserReportRepository.java
@@ -8,8 +8,13 @@ import com.nonononoki.alovoa.entity.User;
 import com.nonononoki.alovoa.entity.user.UserReport;
 
 public interface UserReportRepository extends JpaRepository<UserReport, Long> {
-	UserReport findByUserFromAndUserTo(User userFrom, User userTo);
-	List<UserReport> findByUserFrom(User userFrom);
-	List<UserReport> findByUserTo(User userTo);
-	List<UserReport> findTop20ByOrderByDateAsc();
+    UserReport findByUserFromAndUserTo(User userFrom, User userTo);
+
+    List<UserReport> findByUserFrom(User userFrom);
+
+    List<UserReport> findByUserTo(User userTo);
+
+    List<UserReport> findTop20ByOrderByDateAsc();
+
+    List<UserReport> findByUserToIsNull();
 }
