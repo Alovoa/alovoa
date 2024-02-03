f57e78cd98dbc1731188396a3593bcf5bcd2cdd3
Nho Quy Dinh <nonononoki@gmail.com>
2024-02-03 13:21:56 +0100

    Improve donation-list page

--- src/main/resources/static/js/donate-list.js
+++ src/main/resources/static/js/donate-list.js
@@ -1,13 +1,4 @@
-$(function() {
-	if (localStorage.getItem("twa_playstore")) {
-		$("#donation-links").hide();
-		alert(getText("donate-list.store.warning"));
-	}
-});
-
 function donateInfo() {
 	let text = document.getElementById("donate-notice-1").innerHTML;
-	let text2 = document.getElementById("donate-notice-2").innerHTML;
 	alert(text);
-	alert(text2);
 }
