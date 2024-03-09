582889d8355b53010c8d0cf65afe1206d75b85fe
Nho Quy Dinh <nonononoki@gmail.com>
2024-03-09 21:13:21 +0100

    Update UserSettings.java

--- src/main/java/com/nonononoki/alovoa/entity/user/UserSettings.java
+++ src/main/java/com/nonononoki/alovoa/entity/user/UserSettings.java
@@ -1,5 +1,6 @@
 package com.nonononoki.alovoa.entity.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.nonononoki.alovoa.entity.User;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
@@ -18,6 +19,7 @@ public class UserSettings {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
+    @JsonIgnore
     @OneToOne
     private User user;
 
