a3a4686684b96b67a6671703115a4bf7cc3eb7ef
Nho Quy Dinh <nonononoki@gmail.com>
2024-03-11 20:52:08 +0100

    Revert "Update UserSettingsController.java"
    This reverts commit 64174e6c9baab71184c155083406bd121791e02c.

--- src/main/java/com/nonononoki/alovoa/rest/UserSettingsController.java
+++ src/main/java/com/nonononoki/alovoa/rest/UserSettingsController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/user-settings")
+@RequestMapping("/user/settings")
 public class UserSettingsController {
 
     @Autowired
