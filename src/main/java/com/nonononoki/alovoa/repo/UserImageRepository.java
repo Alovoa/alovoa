9e8fbcc14670755aeb3f85f56e01e7be0d1c0803
Nho Quy Dinh <nonononoki@gmail.com>
2024-04-05 20:31:22 +0200

    [TESTING] Enable caching

--- src/main/java/com/nonononoki/alovoa/repo/UserImageRepository.java
+++ src/main/java/com/nonononoki/alovoa/repo/UserImageRepository.java
@@ -1,5 +1,6 @@
 package com.nonononoki.alovoa.repo;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nonononoki.alovoa.entity.user.UserImage;
@@ -7,6 +8,7 @@ import com.nonononoki.alovoa.entity.user.UserImage;
 import java.util.UUID;
 
 public interface UserImageRepository extends JpaRepository<UserImage, Long> {
+    @Cacheable("UserImageRepository.findByUuid")
     UserImage findByUuid(UUID uuid);
 }
 
