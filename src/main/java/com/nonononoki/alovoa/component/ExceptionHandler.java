6cf723cce8c5df90e308ce773775a92dd50a2f7c
Nho Quy Dinh <nonononoki@gmail.com>
2024-04-13 10:22:54 +0200

    Update ExceptionHandler.java

--- src/main/java/com/nonononoki/alovoa/component/ExceptionHandler.java
+++ src/main/java/com/nonononoki/alovoa/component/ExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.nonononoki.alovoa.component;
 
+import org.apache.catalina.connector.ClientAbortException;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.hibernate.StaleObjectStateException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.core.env.Profiles;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -32,9 +35,11 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
 		String exceptionMessage = ex.getMessage();
 		if (ex instanceof AlovoaException && !env.acceptsProfiles(Profiles.of("dev"))) {
 			LOGGER.error(ExceptionUtils.getMessage(ex));
-		} else {
-			LOGGER.error(ExceptionUtils.getStackTrace(ex));
-		}
+		} else if (!(ex instanceof ObjectOptimisticLockingFailureException)
+                && !(ex instanceof StaleObjectStateException)
+                && !(ex instanceof ClientAbortException)) {
+            LOGGER.error(ExceptionUtils.getStackTrace(ex));
+        }
 		exceptionMessage = exceptionMessage == null ? null : HtmlUtils.htmlEscape(exceptionMessage);
 		return handleExceptionInternal(ex, exceptionMessage, new HttpHeaders(), HttpStatus.CONFLICT, request);
 	}
