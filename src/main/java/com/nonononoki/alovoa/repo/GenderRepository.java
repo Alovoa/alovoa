07bb24553857a873cd788e3778e21dcb824e34f8
Nho Quy Dinh <nonononoki@gmail.com>
2024-12-17 11:16:56 +0100

    Add gender and age parameters to search request

--- src/main/java/com/nonononoki/alovoa/repo/GenderRepository.java
+++ src/main/java/com/nonononoki/alovoa/repo/GenderRepository.java
@@ -1,8 +1,10 @@
 package com.nonononoki.alovoa.repo;
 
+import com.nonononoki.alovoa.entity.user.Gender;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.nonononoki.alovoa.entity.user.Gender;
+import java.util.Collection;
+import java.util.Set;
 
 public interface GenderRepository extends JpaRepository<Gender, Long> {
 	Gender findByText(String text);
